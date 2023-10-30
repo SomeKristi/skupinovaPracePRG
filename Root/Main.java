7b0b0e385031228857129fd863ea40979ac003bd
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-10-30 13:05:50 +0100

    Added logic for ending game and the other game

--- Root/Main.java
+++ Root/Main.java
@@ -1,8 +1,23 @@
+import java.io.Console;
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
-        Hra kamenNuzky = new Hra();
-        int vyberUzivatele = kamenNuzky.nactiVolbuUzivatele();
-        int vyberPC = 1;
-        System.out.println(vyberUzivatele);
+        System.out.println("Chceš si zahrát kámen, nůžky, papír (1), nebo sudá - lichá (2)?");
+        Scanner sc = new Scanner(System.in);
+        int volba = sc.nextInt();
+
+        if (volba == 1){
+            Hra kamenNuzky = new Hra();
+            int vyberUzivatele = kamenNuzky.nactiVolbuUzivatele();
+            // Načti volbu pc
+            // Vyhodnocení
+        }
+        else if (volba == 2){
+            // TODO: Add this thing
+        }
+        else{
+            System.out.println("To není možnost!");
+        }
     }
 }
