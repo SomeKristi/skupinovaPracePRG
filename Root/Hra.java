f6ae4ec34b3442cbd6c0c5a65b9086b171795470
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-10-30 12:21:58 +0100

    Added nactiVolbuUzivatele()

--- Root/Hra.java
+++ Root/Hra.java
@@ -1,10 +1,19 @@
 package Root;
 
+import java.util.Scanner;
+
 public class Hra {
     void vypisVyteze(){
         // Logika zde
     }
-    void nactiVolbuUzivatele(){
-
+    public String nactiVolbuUzivatele(){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("kamen (1), nuzky (2) nebo papir? (3)");
+            String volba = sc.nextLine();
+            if (volba == "1" || volba == "2" || volba == "3"){
+                return volba;
+            }
+        }
+        return "Scanner does not work";
     }
 }
