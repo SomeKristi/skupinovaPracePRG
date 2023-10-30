ed826c078925568ea006269a899fc6eef5448081
SomeKristi <99057275+SomeKristi@users.noreply.github.com>
2023-10-30 11:32:08 +0000

    testing nacteni uzuivatele

--- Root/Main.java
+++ Root/Main.java
@@ -2,6 +2,9 @@ package Root;
 
 class Main{
     public static void main(String[] args) {
-        
+        Hra kamenNuzky = new Hra();
+        String vyberUzivatele = kamenNuzky.nactiVolbuUzivatele();
+        int vyberPC = 1;
+        System.out.println(vyberUzivatele);
     }
 }
